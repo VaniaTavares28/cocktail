bff482e82dfcaa065fb01e6a154ce85478d6afbc
VaniaTavares28 <vania.tavares@studiographene.com>
2022-05-16 12:54:36 +0100

    images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,15 @@
+import cocktailLeft from "./cocktail_left.png";
+import cocktailRight from "./cocktail_right.png";
+import logo from "./logo.png";
+import patreonLogo from "./patreon_logo.png";
+import glasses from "./glasses.png";
+
+const images: Record<string, string> = {
+  cocktailLeft,
+  cocktailRight,
+  glasses,
+  logo,
+  patreonLogo,
+};
+
+export default images;
