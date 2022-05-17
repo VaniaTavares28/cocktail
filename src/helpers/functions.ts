bd7d2faacc7b8ae100df2c6e9c0a7579529dae74
VaniaTavares28 <vania.tavares@studiographene.com>
2022-05-17 10:11:33 +0100

    helper functions

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -2,3 +2,9 @@ export const randomEight = <Type>(array: Type[]): Type[] => {
   const randomIndex = Math.floor(Math.random() * (array.length - 7));
   return [...array].sort(() => Math.random() - 0.5).splice(randomIndex, 8);
 };
+
+export const alphabetGenerator = (): string[] => {
+  return [...Array(26)].map((_elt, index: number) =>
+    String.fromCharCode(index + 97)
+  );
+};
