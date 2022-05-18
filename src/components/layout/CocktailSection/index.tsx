9bb3f49580da0eae11036d631478036e1a7b95dc
VaniaTavares28 <vania.tavares@studiographene.com>
2022-05-18 12:50:34 +0100

    browse grid adjustment

--- src/components/layout/CocktailSection/index.tsx
+++ src/components/layout/CocktailSection/index.tsx
@@ -6,10 +6,12 @@ const CocktailSection = ({
   title,
   cocktails,
   custom,
+  size,
 }: {
   custom?: boolean;
   title: string;
   cocktails: Cocktails | Search;
+  size: number;
 }) => {
   if (cocktails.loading || cocktails.error.hasHappened)
     return (
@@ -25,7 +27,7 @@ const CocktailSection = ({
       <h3>{title}</h3>
       <div className={custom ? "cocktail-browse-grid" : "cocktail-grid"}>
         {cocktails.drinks.map((drink) => (
-          <CocktailCard key={drink.idDrink} size={95} drink={drink} />
+          <CocktailCard key={drink.idDrink} size={size} drink={drink} />
         ))}
       </div>
     </section>
